a6bbcccb51e7b8c7029e00e5aa8bbf94da8db0ac
Hayley Oliver <hcoliver97@gmail.com>
2021-04-06 15:40:56 -0700

    Updated Documentation
    Updated comments in Current Position Patch

--- Current_Position_Patch.py
+++ Current_Position_Patch.py
@@ -4,7 +4,7 @@ Update Current Position with Relevant Job Title
 TODO:
  - file Output
  - documentation
- - checking if it is a current position 
+ - checking if it is a current position
 
 '''
 
@@ -21,44 +21,52 @@ input_filepath = sys.argv[1]
 input_data = pd.read_csv(input_filepath)
 input_df = pd.DataFrame(input_data)
 
-# Function to check org title for
-
+# Function to check job title string for keywords
 def find_key(title):
-    # Position key words
+    # Position key words:
     keys = ['Owner', 'Founder', 'President']
+    # Loop through keywords
     for key in keys:
+        # Check if keyword is contained in title
         if search(key, title):
-            #title found
+            # Keyword found in title
             return True
         else:
-            #title not found, check next title in keys
+            # Keyword not found, check next key
             continue
-    #none of the keys were found in title
+    # None of the keys were found in the title
     return False
 
+# For each lead in input data
 for index in input_df.index:
-    name = input_df['last_name'][index]
+    # There are up to 4 organizations + positions
     for i in [1, 2, 3, 4]:
+        # Check if title is empty (only applicable for org 2 - 4)
         if input_df['organization_title_'+str(i)][index] == 'nan':
-            # no title exists
-            print('NOTE: no further titles to check')
+            # No title exists to check, move to next row
             break
+        # Update current title and organization to that of current org i
         current_title = input_df['organization_title_'+str(i)][index]
         current_org = input_df['organization_'+str(i)][index]
+        # Call find_key method to check if title contains key words.
         if find_key(current_title):
-            # Title contains one of key words
-            print(name + ': ' + current_title)
+            # Returns true if title contains one of key words.
+            # Current title, org are the ones we want.
             break
         else:
+            # Returns false, title does not contain any of key words.
+            # TODO: Error handling?
             continue
 
         # Name didn't contain titles, reset current title to the first org
-        print(name + ': title not found')
+        # TODO: Error handling?
         current_title = input_df['organization_title_1'][index]
         current_org = input_df['organization_1'][index]
 
+    # Update current company and position with the company title and org found
     input_df['current_company_position'][index] = current_title
     input_df['current_company'][index] = current_org
 
-# Output Data to CSV
+# Output Data to CSV file
+# TODO: generaliza file output 
 input_df.to_csv('Brandee_Justus_All_LI_Updated.csv', index = False)
