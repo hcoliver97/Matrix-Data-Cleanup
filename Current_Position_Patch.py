f25eddd85f07302faf3e6bc27fc048d72de1e4dd
Hayley Oliver <hcoliver97@gmail.com>
2021-04-07 12:27:10 -0700

    Docs and minor fixes
    Documentation updated, fixes to columns done.

--- Current_Position_Patch.py
+++ Current_Position_Patch.py
@@ -1,10 +1,13 @@
 '''
 Update Current Position with Relevant Job Title
 
+- Fill in fields indicated by *** UPDATE *** as they are client specific info
+
+
 TODO:
- - file Output
  - documentation
  - checking if it is a current position
+ - making this a modular Function
 
 '''
 
@@ -16,6 +19,10 @@ from os import path
 from re import search
 import Data_Cleanup
 
+# ***UPDATE***
+# Client Name - First_Last
+client = 'Brandee_Justus'
+
 # Read in CC data file specified in commandline and store as DataFrame object
 input_filepath = sys.argv[1]
 input_data = pd.read_csv(input_filepath)
@@ -23,7 +30,8 @@ input_df = pd.DataFrame(input_data)
 
 # Function to check job title string for keywords
 def find_key(title):
-    # Position key words:
+    # *** UPDATE ***
+    # Position key words - different for each client
     keys = ['Owner', 'Founder', 'President']
     # Loop through keywords
     for key in keys:
@@ -67,6 +75,32 @@ for index in input_df.index:
     input_df['current_company_position'][index] = current_title
     input_df['current_company'][index] = current_org
 
+
 # Output Data to CSV file
-# TODO: generaliza file output 
-input_df.to_csv('Brandee_Justus_All_LI_Updated.csv', index = False)
+# TODO: Call Cleanup Script to do this
+# Keep only columns:
+#
+deletion_list = ['id', 'public_id','hash_id', 'member_id', 'sn_member_id',\
+    'sn_hash_id', 'r_member_id', 't_hash_id', 'lh_id', 'full_name', \
+    'original_first_name', 'original_last_name', 'custom_first_name', \
+    'custom_last_name', 'avatar', 'summary', 'address', 'birthday',\
+    'badges_premium', 'badges_influencer', 'badges_job_seeker', \
+    'badges_open_link', 'current_company_custom', \
+    'current_company_custom_position', 'organization_id_1', \
+    'organization_description_1', 'organization_domain_1', \
+    'organization_id_2', 'organization_description_2', 'organization_domain_2',\
+    'organization_id_3', 'organization_description_3', 'organization_domain_3',\
+    'organization_id_4', 'organization_description_4', 'organization_domain_4',\
+    'languages', 'twitters', 'phone_type_1', 'phone_2', 'phone_type_2', \
+    'mutual_first_fullname', 'mutual_second_fullname', 'original_mutual_first_fullname',\
+    'original_mutual_second_fullname', 'custom_mutual_first_fullname',\
+    'custom_mutual_second_fullname','followers','member_distance',\
+    'network_info_connection_count', 'network_info_following', 'third_party_email_1',\
+    'third_party_email_source_1', 'third_party_email_is_valid_1', 'third_party_email_2',\
+    'third_party_email_source_2', 'third_party_email_source_3', \
+    'third_party_email_is_valid_2', 'third_party_email_3','third_party_email_is_valid_3' ]
+
+# Deletes columns specified in list if they exist, ignores error otherwise
+input_df = input_df.drop(deletion_list, axis = 1, errors = 'ignore')
+# TODO: generaliza file output
+input_df.to_csv('John_Grillos_All_LI_Leads.csv', index = False)
---
+++ LI_Workflow_Formatter.py
@@ -0,0 +1,88 @@
+'''
+
+Formatting LinkedIn Leads for new workflow sheet
+
+Input: Filepath to CSV file containing data from LinkedHelper
+
+Output: File containing cleaned data ready to upload to
+        Workflow Google Sheet. File will have same name as original but
+        '_cleaned' at the end of the file name.
+
+This script deletes unnecessary columns and arranges the remaining columns to
+fit the LI Workflow sheet format. Keeps info for 3 organizations, basic lead info,
+tags, etc as shown in column_names list.
+
+This does not distinguish between current and past organizations!
+Only formats the given data for upload.
+
+TODO:
+    - Process and condense some of the columns like start and end date into
+    neater format 'MM/DD/YYYY - Present' or 'MM/DD/YYY - MM/DD/YYYY' in one column?
+    - Make this modular part of Data_Cleanup.py
+
+'''
+
+# Pandas - data analysis module for python
+import sys
+import pandas as pd
+
+# Read contents of csv file specified in commandline input
+filepath = sys.argv[1]
+data = pd.read_csv(filepath)
+
+# Convert CSV data to Data Frame (Pandas object) for easier manipulation
+df = pd.DataFrame(data)
+
+# List of columns to delete
+deletion_list = ['id', 'current_company', 'current_company_position',\
+    'headline','public_id','hash_id', 'member_id', 'sn_member_id',\
+    'sn_hash_id', 'r_member_id', 't_hash_id', 'lh_id', 'full_name', \
+    'original_first_name', 'original_last_name', 'custom_first_name', \
+    'custom_last_name', 'avatar', 'summary', 'address', 'birthday',\
+    'badges_premium', 'badges_influencer', 'badges_job_seeker', \
+    'badges_open_link', 'current_company_custom', \
+    'current_company_custom_position', 'organization_id_1', \
+    'organization_description_1', 'organization_domain_1', \
+    'organization_id_2', 'organization_description_2', 'organization_domain_2',\
+    'organization_id_3', 'organization_description_3', 'organization_domain_3',\
+    'organization_id_4', 'organization_description_4', 'organization_domain_4',\
+    'organization_location_1','organization_location_2','organization_location_3',\
+    'organization_location_4','organization_4','organization_url_4',\
+    'organization_title_4','organization_start_4','organization_end_4',\
+    'organization_website_4','languages', 'twitters', 'phone_type_1',\
+    'phone_type_2','mutual_first_fullname', 'mutual_second_fullname', \
+    'original_mutual_first_fullname', 'original_mutual_second_fullname', \
+    'custom_mutual_first_fullname','custom_mutual_second_fullname','followers',\
+    'member_distance','network_info_connection_count', 'network_info_following',\
+    'third_party_email_1','third_party_email_source_1', \
+    'third_party_email_is_valid_1', 'third_party_email_2',\
+    'third_party_email_source_2', 'third_party_email_source_3', \
+    'third_party_email_is_valid_2', 'third_party_email_3',\
+    'third_party_email_is_valid_3', 'mutual_count','language_1',\
+    'language_proficiency_1','language_2',\
+    'language_proficiency_2','language_3','language_proficiency_3']
+# Deletes columns specified in list if they exist, ignores error otherwise
+df = df.drop(deletion_list, axis = 1, errors = 'ignore')
+
+# List of columns in order that appears on workflow sheet
+column_names = ['connected_at', 'profile_url','tags','email','first_name',\
+	'last_name','location_name','industry',	'organization_1','organization_url_1',\
+    'organization_title_1',	'organization_start_1',	'organization_end_1',\
+    'organization_website_1','organization_2','organization_url_2',\
+    'organization_title_2',	'organization_start_2',	'organization_end_2',\
+    'organization_website_2','organization_3','organization_url_3',	\
+    'organization_title_3','organization_start_3','organization_end_3',\
+    'organization_website_3','skills','phone_1','phone_2','website_1','website_2',\
+    'website_3']
+
+# Re-order the columns according to above list.
+# Moves Connected At and Tags to position A and C
+df = df.reindex(columns = column_names)
+
+# Generate new filepath to store the cleaned file
+# [original file name]_Cleaned.csv in same directory as original file path
+temp = filepath.find('.csv')
+new_filepath = filepath[:temp] + '_Cleaned' + filepath[temp:]
+
+# Output data to new filepath as CSV
+df.to_csv(new_filepath, index=False)
